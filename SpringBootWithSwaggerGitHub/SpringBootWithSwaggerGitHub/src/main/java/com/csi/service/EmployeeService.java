fc70608b927550c09fd66343698da8312b3c4ae4
roshni-bhavsar <roshnibhavsar2911997@gmail.com>
2021-11-26 15:08:32 +0530

    added getdatabyusing any input functionality

---
+++ SpringBootWithSwaggerGitHub/SpringBootWithSwaggerGitHub/src/main/java/com/csi/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.csi.service;
+
+import com.csi.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public void addData(Employee employee);
+
+    public List<Employee> getDataByUsingAnyInput(Employee employee);
+
+}
---
+++ SpringBootWithSwaggerGitHub/SpringBootWithSwaggerGitHub/src/main/java/com/csi/service/EmployeeServiceImpl.java
@@ -0,0 +1,29 @@
+package com.csi.service;
+
+import com.csi.dao.EmployeeDao;
+import com.csi.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+
+    @Override
+    public void addData(Employee employee) {
+
+        employeeDao.addData(employee);
+
+    }
+
+    @Override
+    public List<Employee> getDataByUsingAnyInput(Employee employee) {
+
+        return employeeDao.getDataByUsingAnyInput(employee);
+    }
+}
